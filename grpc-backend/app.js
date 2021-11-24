69eb50f1645bed44c47444fce869a189ff20f02b
Jeff Hollan <jeffhollan@outlook.com>
2021-11-24 14:00:46 -0800

    initial - grpc back

---
+++ grpc-backend/app.js
@@ -0,0 +1,35 @@
+
+var PROTO_PATH = __dirname + '/../protos/greet.proto';
+
+var grpc = require('@grpc/grpc-js');
+var protoLoader = require('@grpc/proto-loader');
+var packageDefinition = protoLoader.loadSync(
+    PROTO_PATH,
+    {keepCase: true,
+     longs: String,
+     enums: String,
+     defaults: true,
+     oneofs: true
+    });
+var greet_proto = grpc.loadPackageDefinition(packageDefinition).greet;
+
+/**
+ * Implements the SayHello RPC method.
+ */
+function sayHello(call, callback) {
+  callback(null, {message: 'Hello ' + call.request.name});
+}
+
+/**
+ * Starts an RPC server that receives requests for the Greeter service at the
+ * sample server port
+ */
+function main() {
+  var server = new grpc.Server();
+  server.addService(greet_proto.Greeter.service, {sayHello: sayHello});
+  server.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), () => {
+    server.start();
+  });
+}
+
+main();
